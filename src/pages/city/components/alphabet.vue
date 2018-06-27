8a3c16ff7d77abacf24bb53e92b6b424ebd4a27b
807334768@qq.com <807334768@qq.com>
2018-06-27 20:15:15 +0800

    add alphabet

---
+++ src/pages/city/components/alphabet.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<ul class="list">
+			<li class="item">A</li>
+			<li class="item">A</li>
+			<li class="item">A</li>
+		</ul>
+	</div>
+	
+</template>
+<script>
+	export default {
+		name:'CityAlphabet'
+	}
+</script>
+<style lang='stylus' scoped>
+.list
+	display:flex
+	flex-direction:column
+	justify-content:center
+	position:absolute
+	top:1.8rem
+	right:0
+	left:auto
+	bottom:0
+	width:.4rem
+	.item
+		text-align:center
+		line-height:.44rem
+		color:$bgColor
+</style>
